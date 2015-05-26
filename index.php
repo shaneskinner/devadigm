52d0fb0ba009e9e05c55d9ab63caf10c17b19a13
Shane Skinner <shane@shaneskinner.com>
2015-05-26 15:15:44 -0400

    Started creating the home page
    Created the outline for the index.html page of www.devadigm.com

---
+++ index.php
@@ -0,0 +1,63 @@
+﻿<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <!-- 
+         Project:		    Devadigm.com
+         File Name:		 index.php
+         Author:		    Shane Skinner
+         Date Created:	 04-10-2015
+	      Last Revised:	 05-26-2015
+         Comments:       Home page for www.devadigm.com
+      -->
+
+      <meta charset="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="author" content="Devadigm | Shane Skinner" />
+      <meta name="description" content="x" />
+      <meta name="keywords" content="x" />
+      <meta name="classification" content="x" />
+      <meta name="owner" content="Devadigm | Shane Skinner" />
+      <meta name="reply-to" content="media@devadigm.com" />
+
+      <title>Devadigm</title>
+
+      <link href="x" rel="stylesheet" type="text/css" media="screen" />
+      <link href="x" rel="stylesheet" type="text/css" media="screen" />
+   </head>
+
+   <body>
+      <header>
+         <section id="connectHeader">
+            <aside>Let's Connect! <a href="https://twitter.com/devadigm" alt="https://twitter.com/devadigm" 
+            target="_blank">@devadigm</a></aside>
+            <aside>Sign-up for our free Small Business newsletter:</aside>
+         </section>
+         <aside>
+            <?php include "php/coverImage.php";?>
+         </aside>
+         <article class="navMenu">
+            <?php include "php/mainNavMenu.php";?>
+         </article>
+      </header>
+      <section>
+         <h1>Welcome to Devadigm.com!</h1>
+         <h2>We help small businesses and entrepreneurs focus on growing their business and customer base by managing their 
+            web projects.</h2>
+      </section>
+      <section>
+         <article>Website Development and Design</article>
+         <article>Social Media and SEO</article>
+         <article>Project Management</article>
+      </section>
+
+      <footer>
+         <aside>Email: <br /> Phone: <br /> Hyannis, MA, USA</aside>
+         <aside>Devadigm</aside>
+         <aside>Proud member of the Cape Cod Young Professionals!</aside>
+      </footer>
+      
+<!-- JavaScript -->
+
+   </body>
+</html>
